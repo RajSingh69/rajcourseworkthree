d91e15542490f7b1fdd16ad63fc378babea56f23
RajSingh69 <rajanbhamra02@gmail.com>
2024-05-09 12:55:19 +0100

    I have improved the look of the create a new post

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,74 +1,101 @@
 @extends('layouts.home')
 
-@section('title', 'Create A New Post')
+@section('title', 'Create a New Post')
 
 @section('content')
 
     <style>
-
-        h1 {
-            font-size: 20px;
-            color: #F56476;
-            font-family: Garamond;
-            text-align: center;
-        }
-
         body {
             background-color: #344055;
             font-family: Helvetica;
-            }
+            margin: 0;
+            padding: 0;
+        }
+
+        .container {
+            max-width: 600px;
+            margin: 0 auto;
+            padding: 20px;
+            background-color: #2E3747;
+            border-radius: 8px;
+        }
+
+        h1 {
+            color: #F3A712;
+            text-align: center;
+            font-size: 26px;
+            font-family: Garamond;
+            margin-bottom: 10px;
+        }
 
         h6 {
             font-size: 20px;
             color: #A5C4D4;
             font-family: Arial;
             text-align: center;
+            margin-top: 10px;
         }
 
         a {
+            display: block;
             font-size: 20px;
             color: #FFBC42;
             font-family: 'Comic Sans MS', cursive;
             font-style: italic;
             text-align: center;
-
+            margin-top: 20px;
+            text-decoration: none;
         }
 
-        input {
-            font-size: 20px;
+        input[type="text"] {
+            width: calc(100% - 20px);
+            padding: 10px;
+            margin: 10px auto;
+            display: block;
+            border: 1px solid #A26769;
+            border-radius: 4px;
+            font-size: 18px;
             color: #A26769;
             font-family: 'Comic Sans MS';
             font-style: italic;
             text-align: center;
-
+            box-sizing: border-box;
         }
 
+        input[type="submit"] {
+            width: 100%;
+            padding: 10px;
+            margin: 10px auto;
+            display: block;
+            background-color: #FFBC42;
+            border: none;
+            border-radius: 4px;
+            font-size: 20px;
+            color: #2E3747;
+            font-family: 'Comic Sans MS';
+            font-style: italic;
+            text-align: center;
+            cursor: pointer;
+            box-sizing: border-box;
+        }
 
-
-
+        input[type="submit"]:hover {
+            background-color: #F56476;
+        }
     </style>
 
+    <div class="container">
+        <h1>--- Create A New Post --- </h1>
+        <form method="POST" action="{{ route('posts.store') }}">
+            @csrf
 
+            <h6>Title: <input type="text" name="postTitle"></h6>
+            <h6>Content: <input type="text" name="postContent"></h6>
+            <h6>Category: <input type="text" name="postCategory"></h6>
 
-
-
-    <form method = "POST" action= " {{ route('posts.store') }} ">
-
-    @csrf
-
-    <h6> Title: <input type= "text" name = "postTitle"> </h6>
-    <h6> Content: <input type= "text" name = "postContent"> </h6>
-    <h6> Category: <input type= "text" name = "postCategory"> </h6>
-
-    <input type = "submit" value = "Submit Post Here!">
-    <a href = " {{ route('posts.index') }} "> Cancel Choice Here! </a>
-
-    </form>
-
-
-
-
-
-
+            <input type="submit" value="Submit Post Here!">
+            <a href="{{ route('posts.index') }}">Cancel Choice Here!</a>
+        </form>
+    </div>
 
 @endsection
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -5,20 +5,23 @@
 @section('content')
     <style>
         body {
-            background-color: #344055;
+            background-color: #1F2633;
             font-family: Helvetica;
+            margin: 0;
+            padding: 0;
         }
 
-
         h1 {
-            color: #FFBC42;
+            color: #FCFCFC;
             text-align: center;
             font-size: 26px;
             font-family: Garamond;
+            margin-bottom: 10px;
         }
+
         a {
             font-size: 20px;
-            color: #FFBC42;
+            color: #D2F898;
             font-family: 'Comic Sans MS', cursive;
             font-style: italic;
             font-weight: bold;
@@ -26,65 +29,139 @@
         }
 
         h2 {
-            text-align: center;
             font-size: 18px;
-            color: #FFBC42;
+            color: #F3A712;
+            margin-top: 30px;
+            margin-bottom: 20px;
             font-family: Helvetica;
+            text-align: center;
+
 
         }
 
+        /* If not youre more than welcome to.... */
         h3 {
             text-align: center;
-            font-size: 18px;
-            color: #DDC3D0;
+            font-size: 16px;
+            color: #FCFCFC;
+            font-family: Arial;
+            margin-top: 40px;
+            margin-bottom: 40px;
+        }
+
+        /* Post title..... */
+        h4 {
+            text-align: center;
+            font-size: 20px;
+            color: #FCFCFC;
+            font-weight: bold;
             font-family: Helvetica;
         }
 
+        /* Here are the commetns for this post */
+        h5 {
+            font-size: 18px;
+            color: #A7CECB;
+            font-style: italic;
+            font-family: Arial;
+        }
+
+        /* Post content.... */
         p {
-            font-size: 16px;
-            color: #8AC926;
+            font-size: 18px;
+            color: #FCFCFC;
+
+            font-family: Arial;
+        }
+
+        li {
+            font-size: 18px;
+            color: #A7CECB;
             font-family: Arial;
         }
 
 
-        
+
+        .container {
+            max-width: 800px;
+            margin: 0 auto;
+            padding: 20px;
+        }
+
+        .create-post {
+            text-align: center;
+        }
+
+        .post {
+            background-color: #2E3747;
+            border-radius: 8px;
+            padding: 20px;
+            margin-bottom: 20px;
+        }
+
+        .post-title {
+            font-size: 24px;
+            color: #FCFCFC;
+            text-align: center;
+            margin-bottom: 10px;
+        }
+
+        .post-content {
+            font-size: 18px;
+            color: #FCFCFC;
+            margin-bottom: 10px;
+        }
+
+        .comments {
+            margin-top: 10px;
+        }
+
+        .comment {
+            font-size: 16px;
+            color: #A7CECB;
+            margin-bottom: 5px;
+        }
+
+        .footer {
+            margin-top: 20px;
+            text-align: center;
+        }
     </style>
 
-    <h1> All beautiful posts are here! Please click on them to see the amazing story they tell! <br> <br> </h1>
+    <div class="container">
+        <h1>All beautiful posts are here! Please click on them to see the amazing story they tell!</h1>
+        <h2>Would you like to add your story to the world databank? Here's your chance!</h2>
+        <div class="create-post">
+            <a href="{{ route('posts.create') }}">***Create A New Post***</a>
+        </div>
+        <h3>If not, you're more than welcome to look at others.<br>Please view until your heart is content!<br>Don't forget to look at your creation at the bottom!</h3>
 
-    <h2> Would you like to add your story to the world databank? Here's your chance! <br> <br> </h2>
-    <centre> <a href="{{ route('posts.create') }}"> ***Create A New Post***  </a> </centre>
+        @foreach ($posts as $post)
 
-    <h3> If not, you're more than welcome to look at others. <br> Please view until your heart is content! <br> Don't forget to look at your creation at the bottom! <br> <br> </h3>
+            <div class="post">
 
-    @foreach ($posts as $post)
-        <h2> <a href="/posts/{{$post->id}}"> {{ $post->postTitle }} </a> </h2>
-        <p> Post Content: {{ $post->postContent }} </p>
-        
+                <div class="post-title">
+                    <b>Post Title:</b><br>
+                    <a href="/posts/{{$post->id}}">{{$post->postTitle}}</a>
+                </div>
 
-        <h2> Here are the comments for this post </h2>
+                <div class="post-content">
+                    <b>Post Content:</b><br>
+                    {{$post->postContent}}
+                </div>
 
-        @foreach($post->comments as $comment)     
-        <li>{{ $comment->commentContent }}</li>       
+                <div class="comments">
+                    <h5>Here are the comments for this post:</h5>
+                    @foreach($post->comments as $comment)
+                        <div class="comment">{{$comment->commentContent}}</div>
+                    @endforeach
+
+                </div>
+            </div>
         @endforeach
 
-        
-
-        <hr>
-
-    @endforeach
-
-
-
-
-
-
-
-
-
-    <div class = "footer">
-    {{ $posts -> onEachSide(5)->links()}}
+        <div class="footer">
+            {{$posts->onEachSide(5)->links()}}
+        </div>
     </div>
-
-
 @endsection
